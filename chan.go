aef470ba046fac8bd1601639a35813435553340b
Zach <zachctown@gmail.com>
2014-02-25 22:32:10 -0500

    Added html templating

--- chan.go
+++ chan.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-		"fmt"
 		"log"
 		_ "github.com/mattn/go-sqlite3"
 		"database/sql"
 		"net/http"
+		"html/template"
 )
-
-	
 
 type ImageBoard struct {
 	db *sql.DB
 }
+
+type Post struct {
+	threadID int
+	subject string
+	name string
+	text string
+	date_posted string
+}
+
+func (p *Post) ThreadID()   int    { return p.threadID    }
+func (p *Post) Subject()    string { return p.subject     }
+func (p *Post) Name()       string { return p.name        }
+func (p *Post) Text()       string { return p.text        }
+func (p *Post) DatePosted() string { return p.date_posted }
 
 func main(){
 	db, err := sql.Open("sqlite3", "./imageboard.db")
@@ -21,7 +33,7 @@ func main(){
 	}
 	i := &ImageBoard{db}
 
-	//i.newPost("Post Subject method test", "Anon", "First method post", 3)
+	i.newPost("Post Subject method test", "Anon", "First method post", 2)
 	http.HandleFunc("/", i.latestThreads)
 	http.ListenAndServe(":8080", nil)
 }
@@ -44,10 +56,11 @@ func (i *ImageBoard) newPost(subject string, name string, text string, thread_id
 }
 
 func (i *ImageBoard) latestThreads(w http.ResponseWriter, r *http.Request){
-	latest_threads, err := i.db.Query("SELECT * FROM latest_threads")
+	latest_threads, err := i.db.Query("SELECT * FROM latest_threads GROUP BY thread_id")
 	if err != nil{
 		log.Fatal(err)
 	}
+	posts := []Post{}
 	defer latest_threads.Close()
 	for latest_threads.Next(){
 		var threadID int
@@ -56,7 +69,23 @@ func (i *ImageBoard) latestThreads(w http.ResponseWriter, r *http.Request){
 		var text string
 		var date_posted string
 		latest_threads.Scan(&threadID, &subject, &name, &text, &date_posted) 
-		fmt.Fprintf(w, "<h1>%d</h1><h1>%s</h1><h1>%s</h1><p>%s</p><p>%s</p>", threadID, subject, name, text, date_posted)
+		post := Post{
+			threadID,
+			subject,
+			name,
+			text,
+			date_posted,
+		}
+		posts = append(posts, post)
 	}
-	latest_threads.Close()
+	t, error := template.ParseFiles("thread.html")
+	if error != nil{
+		log.Fatal(error)
+	}
+	t.Execute(w, posts)
 }
+
+/*
+SELECT * FROM latest_threads GROUP BY thread_id;
+SELECT * FROM latest_threads
+*/
